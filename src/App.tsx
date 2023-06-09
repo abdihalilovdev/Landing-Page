944d740dd3acd9860d31b0b081e40f3a1a02f89f
abdihalilovdev <zabdihalilov23@gmail.com>
2023-06-09 11:35:21 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header/Header";
+import {Route, Routes} from "react-router-dom";
+import Hero from "./Components/Hero/Hero";
+import Admin from "./Components/Admin/Admin";
+import Menu from "./Components/Menu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+        <Routes>
+            <Route path={'/hero'} element={ <Hero/> }/>
+            <Route path={'/'} element={ <Admin/> }/>
+            <Route path={'/menu'} element={ <Menu/> }/>
+        </Routes>
     </div>
   );
 }
